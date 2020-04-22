2f60d8cd812c52934b275fc7a1d437b6db90bb25
Dominic Bonney <bonneydominic44@gmail.com>
2020-04-22 12:19:42 +0000

    first commit

---
+++ resources/views/layout/masster.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Aped Construction Company</title>
+	@include('include.header')
+</head>
+<body>
+	<div>
+		@yield('nav')
+	</div>
+	<div>
+		@yield('body')
+	</div>
+	<div>
+		@yield ('footer')
+	</div>
+
+</body>
+</html>
